d637844b455c4b1f2b5581a482ee34b4db7a363d
HOANGNM <hoangnghiem205@gmail.com>
2019-01-13 16:44:21 +0700

    slide 3 - if-else, while,for + source

---
+++ source/Bai3/src/com/java/demo/Main.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.java.demo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pc
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int sum = 0;
+        
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 != 0) continue;
+            System.out.println(i);
+        }
+        System.out.println(sum);
+//        Scanner input = new Scanner(System.in);
+//        for (int i = 0; i < 100; i++) {
+//            System.out.print("Enter a number > ");
+//            int number = input.nextInt();
+//            if (number == 500) {
+//                break;
+//            }
+//        }
+        
+        
+        
+//        for (int i = 0; i < 10; i++) {
+//            for (int j = 0; j < i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println("");
+//        }
+        
+//        int sum = 0;
+//        int i = 0;
+//        
+//        do {
+//            sum += i;
+//            i += 2;
+//        } while(i <= 10);
+        
+        
+//        while (i <= 10) {
+//            sum += i;
+//            i+=2;
+//        }
+//        System.out.println(sum);
+        
+        
+//        int sum = 0;
+//        for (int i = 0; i < 11; i+=2) {
+//            //sum = sum + i;
+//            sum += i;
+//        }
+//        //0 2 4 6 8 10
+//        System.out.println("Tong " + sum);
+        
+//        int day = 4;
+//        
+//        switch (day) {
+//            case 0:
+//                System.out.println("Thu hai");
+//                break;
+//            case 1:
+//                System.out.println("Thu ba");
+//                break;
+//            case 2:
+//                System.out.println("Thu tu");
+//                break;
+//            case 3:
+//                System.out.println("Thu nam");
+//                break;
+//            case 4:
+//                System.out.println("Thu sau");
+//                break;
+//            case 5:
+//                System.out.println("Thu bay");
+//                break;
+//            case 6:
+//                System.out.println("Thu CN");
+//                break;
+//            default:
+//                System.out.println("Khong hop le");
+//        }
+//        
+    }
+    
+}
