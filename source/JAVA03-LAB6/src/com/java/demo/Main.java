dc66745fdb3eb31adf607e424c3edd30196b32d9
HOANGNM <hoangnghiem205@gmail.com>
2019-03-09 17:17:50 +0700

    update source MyDate

---
+++ source/JAVA03-LAB6/src/com/java/demo/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.java.demo;
+
+
+/**
+ *
+ * @author pc
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            // TODO code application logic here
+            MyDate d = new MyDate(1, 1, 2019);
+            d.addDay(61);
+            d.print(0);
+            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+    }
+    
+}
